6816179f9cded33d1a1898e0ee1dc9be6227348c
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-11-16 00:05:21 -0300

    Styling Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,89 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/camisa-flamengo-i-1920-sn-torcedor-c-patrocinio-adidas-masculina/68/COL-7378-068/COL-7378-068_zoom1.jpg" alt="Produto" />
+        <strong>Camisa Oficial do Flamengo</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span className="buttonText">ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/camisa-flamengo-i-1920-sn-torcedor-c-patrocinio-adidas-masculina/68/COL-7378-068/COL-7378-068_zoom1.jpg" alt="Produto" />
+        <strong>Camisa Oficial do Flamengo</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span className="buttonText">ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/camisa-flamengo-i-1920-sn-torcedor-c-patrocinio-adidas-masculina/68/COL-7378-068/COL-7378-068_zoom1.jpg" alt="Produto" />
+        <strong>Camisa Oficial do Flamengo</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span className="buttonText">ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/camisa-flamengo-i-1920-sn-torcedor-c-patrocinio-adidas-masculina/68/COL-7378-068/COL-7378-068_zoom1.jpg" alt="Produto" />
+        <strong>Camisa Oficial do Flamengo</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span className="buttonText">ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/camisa-flamengo-i-1920-sn-torcedor-c-patrocinio-adidas-masculina/68/COL-7378-068/COL-7378-068_zoom1.jpg" alt="Produto" />
+        <strong>Camisa Oficial do Flamengo</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span className="buttonText">ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/camisa-flamengo-i-1920-sn-torcedor-c-patrocinio-adidas-masculina/68/COL-7378-068/COL-7378-068_zoom1.jpg" alt="Produto" />
+        <strong>Camisa Oficial do Flamengo</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span className="buttonText">ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }

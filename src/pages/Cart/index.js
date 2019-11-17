935658f2512774bd047e7dbae1bea7a76590677f
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-11-17 18:05:10 -0300

    Updating items from cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -11,7 +11,16 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart }) {
+function Cart({ cart, removeFromCart, updateAmount }) {
+  // Para incrementar ou decrementar quantidades de items
+  function increment(product) {
+    updateAmount(product.id, product.amount + 1);
+  };
+
+  function decrement(product) {
+    updateAmount(product.id, product.amount - 1);
+  };
+
   return (
     <Container>
       <ProductTable>
@@ -39,11 +48,11 @@ function Cart({ cart, removeFromCart }) {
               </td>
               <td>
                 <div>
-                  <button type="button">
+                  <button type="button" onClick={() => decrement(product)}>
                     <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
                   <input type="text" readOnly value={product.amount} />
-                  <button type="button">
+                  <button type="button" onClick={() => increment(product)}>
                   <MdAddCircleOutline size={20} color="#7159c1" />
                   </button>
                 </div>

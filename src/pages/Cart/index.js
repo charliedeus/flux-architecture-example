b5a60df4fbaf77173253e0f8ef4437f06a78df25
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-11-17 14:21:23 -0300

    Styling Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart Page</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTDE</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/camisa-flamengo-i-1920-sn-torcedor-c-patrocinio-adidas-masculina/68/COL-7378-068/COL-7378-068_zoom1.jpg"
+                alt=""
+              />
+            </td>
+            <td>
+              <strong>Camisa do Hepta!</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }

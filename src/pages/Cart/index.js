43dbec1bbf05e6b116b308fb6551b14483d59bff
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-11-19 22:15:08 -0300

    Updating in stock

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -13,14 +13,14 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, total, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
   // Para incrementar ou decrementar quantidades de items
   function increment(product) {
-    updateAmount(product.id, product.amount + 1);
+    updateAmountRequest(product.id, product.amount + 1);
   };
 
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1);
+    updateAmountRequest(product.id, product.amount - 1);
   };
 
   return (
